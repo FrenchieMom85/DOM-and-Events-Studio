fb913b5346043b126237543ec464ce2f9c81b4a0
amfzn503 <amfzn503@msn.com>
2020-03-12 20:58:46 -0500

    beginning work on studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function init(){
+    const takeOffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    takeOffButton.addEventListener("click", function (event){
+        let confirm = window.confirm("Confirm that the shuttle is ready for takeoff");
+        let flightStatus = document.getElementById("flightStatus");
+        if (confirm){
+            flightStatus.innerHTML = "Shuttle in Flight";
+            let shuttleBackground = document.querySelector("#shuttleBackground");
+            shuttleBackground.style.background = "blue";
+        }
+    });
+
+}
+
