b91571821d8ed947b3932780385baf4fcfe65a6c
amfzn503 <amfzn503@msn.com>
2020-03-15 11:21:16 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
-function init(){
+window.addEventListener('load', function(){
+    // commands
     const takeOffButton = document.getElementById("takeoff");
-    const flightStatus = document.getElementById("flightStatus");
-    const landButton = document.getElementById("landing")
+    const landButton = document.getElementById("landing");
     const abortButton = document.getElementById("missionAbort");
+    // rocket
+    const rocket = document.getElementById("rocket");
+    // move controls
     const directions = document.getElementsByTagName(button)
+
+    const shuttleBackground = document.getElementById("shuttleBackground")
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleHeight = document.getElementById("shuttleHeight");
     takeOffButton.addEventListener("click", function (event){
         let confirm = window.confirm("Confirm that the shuttle is ready for takeoff");
-        let flightStatus = document.getElementById("flightStatus");
-        let shuttleHeight = document.getElementById("spaceShuttleHeight");
         if (confirm){
             flightStatus.innerHTML = "Shuttle in Flight";
-            let shuttleBackground = document.querySelector("#shuttleBackground");
             shuttleBackground.style.background = "blue";
             shuttleHeight.innerHTML = "10,000"
         }
     });
     landButton.addEventListener("click", function (event){
         confirm = window.confirm("The shuttle is landing. Landing gear engaged");
-        let shuttleHeight = document.getElementById("spaceShuttleHeight");
-        event.stopPropagation()
         if (confirm){
             flightStatus.innerHTML = "The shuttle has landed";
             shuttleBackground = document.querySelector("#shuttleBackground");
@@ -28,8 +30,6 @@ function init(){
     });
     abortButton.addEventListener("click", function (event){
         confirm = window.confirm("Confirm that you want to abort the mission");
-        let shuttleHeight = document.getElementById("spaceShuttleHeight");
-        event.stopPropagation()
         if (confirm){
             flightStatus.innerHTML = "Mission aborted";
             shuttleBackground = document.querySelector("#shuttleBackground");
@@ -37,11 +37,10 @@ function init(){
             shuttleHeight.innerHTML = "0"
         }
     });
-    directions.addEventListener("click", function (event){
-        let shuttleHeight = document.getElementById("spaceShuttleHeight");
-        event.stopPropagation()
+    // directions.addEventListener("click", function (event){
         
-    });
+        
+    // });
 
 }
 
